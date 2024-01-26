367f0b5bb16885184b55081570a2600d9727a0dc
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-01-26 12:52:38 +0530

    aboutpage update

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -12,29 +12,53 @@ const AboutSection = () => {
           <h2 className='aboutHeading'>Welcome to HOMAID</h2>
           <p className='aboutpara'>
             At HOMAID, we are committed to enhancing the quality of your life by providing exceptional home services. 
-            Our journey began with a simple yet profound idea – to make your home a haven of comfort and convenience.
-          </p>
-          <p className='aboutpara'>
+            Our journey began with a simple yet profound idea to make your home a haven of comfort and convenience. 
             HOMAID offers a range of services tailored to meet your unique needs. From skilled cooks who can whip up culinary delights 
             to reliable maids dedicated to maintaining a pristine living space, we are your trusted partners in home management.
           </p>
           <p className='aboutpara'>
-            Our team of professionals is driven by a passion for service excellence. We understand that your home is more than just 
-            a place; it's a reflection of your lifestyle and personality. That's why we go the extra mile to ensure that our services 
-            not only meet but exceed your expectations.
-          </p>
-          <p className='aboutpara'>
-            What sets HOMAID apart is our unwavering commitment to customer satisfaction. We take pride in building lasting relationships 
-            with our clients, earning their trust through reliability, integrity, and a relentless pursuit of perfection.
-          </p>
-          <p className='aboutpara'>
+            Our team of professionals is driven by a passion for service excellence. We understand that your home is more than just a place; 
+            it's a reflection of your lifestyle and personality. That's why we go the extra mile to ensure that our services not only meet but 
+            exceed your expectations. What sets HOMAID apart is our unwavering commitment to customer satisfaction. We take pride in building lasting 
+            relationships with our clients, earning their trust through reliability, integrity, and a relentless pursuit of perfection.
             Whether you are a busy professional in need of a helping hand or a family seeking support in managing your household, 
             HOMAID is here to make your life easier. Explore our services, and let us take care of the details so you can focus on 
-            what matters most – enjoying a comfortable and harmonious home.
+            what matters most enjoying a comfortable and harmonious home.
           </p>
           <p className='aboutpara'>
             Thank you for choosing HOMAID. We look forward to serving you and becoming an integral part of your home story.
           </p>
+        </div>
+        <div className="about-us-container">
+          <h2 className='aboutHeading'>Why HOMAID?</h2>
+          <p className='aboutpara'></p>
+          <p className='aboutpara'></p>
+          <p className='aboutpara'></p>
+        </div>
+        <div className='about-us-container'>
+          <h2 className='aboutHeading'>The Team</h2>
+          <div className='images-container'>
+            <div className='image-box'>
+              <img src='' alt='' className='image' />
+              <p className='personname'>Name1</p>
+              <p className='personposition'>Position</p>
+            </div>
+            <div className='image-box'>
+              <img src='' alt='' className='image' />
+              <p className='personname'>Name2</p>
+              <p className='personposition'>Position</p>
+            </div>
+            <div className='image-box'>
+              <img src='' alt='' className='image' />
+              <p className='personname'>Name3</p>
+              <p className='personposition'>Position</p>
+            </div>
+            <div className='image-box'>
+              <img src='' alt='' className='image' />
+              <p className='personname'>Name4</p>
+              <p className='personposition'>Position</p>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
