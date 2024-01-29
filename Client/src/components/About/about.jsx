5f9b5269e3ca18bc596afb261119e2bdae9dd2c7
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-01-29 18:16:44 +0530

    Update about.jsx

--- Client/src/components/About/about.jsx
+++ Client/src/components/About/about.jsx
@@ -2,7 +2,7 @@
 import Slider from "react-slick";
 // Import css files
 
-import "./about.css";
+import "/src/components/About/about.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
