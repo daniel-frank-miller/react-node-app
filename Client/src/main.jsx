6c142f73004d96008da01a2a04ad05e64e8ce8e9
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-01-29 18:15:47 +0530

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,6 +1,6 @@
 //  import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import App from "/src/components/App.jsx";
+import "/src/components/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
