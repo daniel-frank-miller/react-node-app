2faff0e692d38023537ea549a78c0062f17a3c65
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-02-02 17:52:40 +0530

    Fixed Responsive pages

--- Client/src/components/AboutSection/AboutSection.jsx
+++ Client/src/components/AboutSection/AboutSection.jsx
@@ -1,119 +1,128 @@
-import Navbar from "/src/components/Navbar/navbar.jsx"
-import Footer from "/src/components/Footer/footer.jsx"
-import '/src/components/AboutSection/AboutSection.css'
+import Navbar from "/src/components/Navbar/navbar.jsx";
+import Footer from "/src/components/Footer/footer.jsx";
+import "/src/components/AboutSection/AboutSection.css";
+import { GiTargetShot } from "react-icons/gi";
 
 const AboutSection = () => {
+  const customNavbar = {
+    backgroundColor: "transparent",
+  };
   return (
-    <>
-      <Navbar />
-      <div className='Outcontainer'>
-        <div className="about-us-container">
-          <h2 className='aboutHeading'>Welcome to HOMAID</h2>
-          <p className='aboutpara'>
-            At HOMAID, we are committed to enhancing the quality of your life by providing exceptional home services. 
-            Our journey began with a simple yet profound idea to make your home a haven of comfort and convenience. 
-            HOMAID offers a range of services tailored to meet your unique needs. From skilled cooks who can whip up culinary delights 
-            to reliable maids dedicated to maintaining a pristine living space, we are your trusted partners in home management.
-          </p>
-          <p className='aboutpara'>
-            Our team of professionals is driven by a passion for service excellence. We understand that your home is more than just a place; 
-            it's a reflection of your lifestyle and personality. That's why we go the extra mile to ensure that our services not only meet but 
-            exceed your expectations. What sets HOMAID apart is our unwavering commitment to customer satisfaction. We take pride in building lasting 
-            relationships with our clients, earning their trust through reliability, integrity, and a relentless pursuit of perfection.
-            Whether you are a busy professional in need of a helping hand or a family seeking support in managing your household, 
-            HOMAID is here to make your life easier. Explore our services, and let us take care of the details so you can focus on 
-            what matters most enjoying a comfortable and harmonious home.
-          </p>
-          <p className='aboutpara'>
-            Thank you for choosing HOMAID. We look forward to serving you and becoming an integral part of your home story.
-          </p>
+    <div className="AboutSection-Container prevent-select">
+      <Navbar style={customNavbar} />
+      <div className="aboutMain-Container">
+        <div className="about-content">
+          <h1 className="about-title">About Us</h1>
+          <p className="about-subtitle">Here is little bit about us</p>
         </div>
-        <div className="about-us-container">
-          <h2 className='aboutHeading'>Why HOMAID?</h2>
-          <p className='aboutpara'>
-            At Homaid, we understand that choosing the right service for your home is a decision that goes beyond a mere transaction; 
-            it's about trust, reliability, and the assurance that your home will be cared for by skilled professionals who prioritize 
-            your comfort and well-being. Here are several reasons why Homaid stands out as the preferred choice for your maid and cook services:
-          </p>
-          <h3 className='miniheading'>1. Excellence in Service</h3>
-          <p >
-            Homaid is synonymous with excellence. Our commitment to delivering top-notch service is unwavering. We meticulously select and train our 
-            team of maids and cooks to ensure they meet the highest standards of professionalism, skill, and integrity. 
-            Your satisfaction is our priority, and we go the extra mile to exceed your expectations.
-          </p>
-          <h3 className='miniheading'>2. Skilled and Trustworthy Professionals</h3>
-          <p>
-            When you invite someone into your home, trust is paramount. Homaid prides itself on having a team of skilled and trustworthy professionals. 
-            Our maids are not just cleaners; they are caretakers of your living space, ensuring it remains a haven of comfort and hygiene. 
-            Our cooks are culinary experts who transform your kitchen into a place where delicious meals come to life.
-          </p>
-          <h3 className='miniheading'>3. Customized Services to Suit Your Lifestyle</h3>
-          <p>
-            We recognize that every home is unique, and so are its needs. Homaid offers customized services tailored to fit 
-            seamlessly into your lifestyle. Whether you require a maid for regular cleaning or a cook for special occasions, our 
-            flexible service packages cater to your specific requirements. Your convenience is our priority.
-          </p>
-          <h3 className='miniheading'>4. Transparent and Affordable Pricing</h3>
-          <p>
-            Homaid believes in transparency. Our pricing is clear, straightforward, and free from hidden charges. 
-            We understand the importance of budgeting for home services, and we strive to provide affordability 
-            without compromising on the quality of our offerings. With Homaid, you get value for your investment.
-          </p>
-          <h3 className='miniheading'>5. Reliability You Can Count On</h3>
-          <p>
-            Consistency is key in home services, and Homaid is synonymous with reliability. We take pride in being a 
-            service provider you can count on. Whether it's the regular cleaning schedule or the preparation of a special meal, 
-            you can trust Homaid to be there, ensuring your home remains in optimal condition.
-          </p>
-          <h3 className='miniheading'>6. Commitment to Customer Satisfaction</h3>
-          <p>
-            Homaid is dedicated to achieving customer satisfaction at every touchpoint. We actively seek feedback to improve 
-            our services continually. Your experience with Homaid matters to us, and we are committed to making your journey 
-            with us as smooth and enjoyable as possible.
-          </p>
-          <h3 className='miniheading'>7. A Holistic Approach to Home Management</h3>
-          <p>
-            Beyond just cleaning and cooking, Homaid adopts a holistic approach to home management. We aim to alleviate 
-            the stress associated with maintaining a home, allowing you to focus on the things that matter most to you. 
-            Our services are designed to contribute to the overall well-being of your household.
-          </p>
+      </div>
+      <div className="TeamDetails">
+        <h2 className="TeamDetails-heading">
+          Welcome to Homaid, where your home is our priority.
+        </h2>
+        <div className="members">
+          <div>
+            <img
+              src="/src/assets/draft-team-img.png"
+              alt="name"
+              className="team-img"
+            />
+            <p className="team-name">Name</p>
+            <p className="teamMember-position">----Position----</p>
+          </div>
+          <div>
+            <img
+              src="/src/assets/draft-team-img.png"
+              alt="name"
+              className="team-img"
+            />
+            <p className="team-name">Name</p>
+            <p className="teamMember-position">----Position----</p>
+          </div>
+          <div>
+            <img
+              src="/src/assets/draft-team-img.png"
+              alt="name"
+              className="team-img"
+            />
+            <p className="team-name">Name</p>
+            <p className="teamMember-position">----Position----</p>
+          </div>
+          <div>
+            <img
+              src="/src/assets/draft-team-img.png"
+              alt="name"
+              className="team-img"
+            />
+            <p className="team-name">Name</p>
+            <p className="teamMember-position">----Position----</p>
+          </div>
         </div>
-        <div className='about-us-container'>
-          <h2 className='aboutHeading'>Conclusion</h2>
-          <p>Choosing Homaid is not just a decision for a service; it's a choice for quality, trust, and a commitment to 
-            a better home life. Discover the difference that Homaid can make in your daily living - where excellence meets 
-            convenience, and your home becomes a sanctuary of comfort and joy.
-          </p>
-        </div>
-        <h1><span className='note'>Welcome to Homaid, where your home is our priority.</span></h1>
-        <div className='about-us-container'>
-          <h2 className='aboutHeading'>The Team</h2>
-          <div className='images-container'>
-            <div className='image-box'>
-              <img src='' alt='' className='image' />
-              <p className='personname'>Name1</p>
-              <p className='personposition'>Position</p>
+      </div>
+      <div className="FAQs">
+        <h1 className="faq-title">
+          Here are some of the freqently asked questions
+        </h1>
+        <div className="questions">
+          <div className="question-Container">
+            <GiTargetShot size={35} className="target-icon" />
+            <div>
+              <h1 className="question">What services does Homaid provide?</h1>
+              <p className="answer">
+                Homaid offers a range of cleaning and cooking services. This
+                includes regular house cleaning, deep cleaning, meal
+                preparation, and personalized chef services.
+              </p>
             </div>
-            <div className='image-box'>
-              <img src='' alt='' className='image' />
-              <p className='personname'>Name2</p>
-              <p className='personposition'>Position</p>
+          </div>
+
+          <div className="question-Container">
+            <GiTargetShot size={35} className="target-icon" />
+            <div>
+              <h1 className="question">
+                How do I book a cleaning or cooking service with Homaid?
+              </h1>
+              <p className="answer">
+                You can easily book a service through the Homaid website. Simply
+                select the type of service you need, choose a date and time, and
+                provide any specific instructions. Homaid will then match you
+                with a qualified professional.
+              </p>
             </div>
-            <div className='image-box'>
-              <img src='' alt='' className='image' />
-              <p className='personname'>Name3</p>
-              <p className='personposition'>Position</p>
+          </div>
+
+          <div className="question-Container">
+            <GiTargetShot size={35} className="target-icon" />
+            <div>
+              <h1 className="question">
+                Are the cleaners and cooks from Homaid trained and vetted?
+              </h1>
+              <p className="answer">
+                Yes, all professionals at Homaid undergo a thorough vetting
+                process, including background checks and training. This ensures
+                that you receive reliable and skilled individuals to assist with
+                your cleaning and cooking needs.
+              </p>
             </div>
-            <div className='image-box'>
-              <img src='' alt='' className='image' />
-              <p className='personname'>Name4</p>
-              <p className='personposition'>Position</p>
+          </div>
+
+          <div className="question-Container">
+            <GiTargetShot size={35} className="target-icon" />
+            <div>
+              <h1 className="question">
+                Can I request a specific cleaner or cook from Homaid?
+              </h1>
+              <p className="answer">
+                Homaid aims to provide consistency in service, and you can
+                request a preferred professional based on availability. However,
+                it&apos;s not guaranteed due to scheduling and other factors.
+              </p>
             </div>
           </div>
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
