1bedf24e5a595343d19aa552c761bd5fe79a4f67
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-02-14 19:34:05 +0530

    updated testimonals and services media queries

--- Client/src/components/Navbar/navbar.jsx
+++ Client/src/components/Navbar/navbar.jsx
@@ -3,10 +3,13 @@ import { Events } from "react-scroll";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 import "/src/components/Navbar/navbar.css";
+import Cookies from "js-cookie";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [scrolling, setScrolling] = useState(false);
+  //const [status,setStatus]=useState(false);
+
   const location=useLocation()
   useEffect(()=>{
     window.scrollTo(0, 0);
@@ -14,6 +17,8 @@ const Navbar = () => {
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  
 
   const handleScroll = () => {
     if (window.scrollY > 0) {
@@ -36,6 +41,31 @@ const Navbar = () => {
 
   const navbarClassName = scrolling ? "navbar scrolled" : "navbar";
 
+  const toggleLogout=()=>{
+    setMenuOpen(!menuOpen);
+    Cookies.remove("jwt_token")
+  }
+
+  const loginBtnStatus=()=>{
+    if(Cookies.get("jwt_token")!==undefined){
+      return (
+        <li>
+          <NavLink to="/login" onClick={toggleLogout}>
+            Logout
+          </NavLink>
+        </li>
+      )
+    }
+    else{
+     return(
+      <li>
+      <NavLink to="/login" onClick={toggleMenu}>
+        Login
+      </NavLink>
+    </li>
+     )
+    }
+  }
 
   return (
     <div className={`${navbarClassName} prevent-select`}>
@@ -70,11 +100,7 @@ const Navbar = () => {
             Contact
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/login" onClick={toggleMenu}>
-            Login
-          </NavLink>
-        </li>
+       {loginBtnStatus()}
       </ul>
     </div>
   );
