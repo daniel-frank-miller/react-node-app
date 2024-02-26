17a00d07d600811e37adc29236d1796cd3ecd8cd
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-02-26 21:51:28 +0530

    video changing in hero section added

--- Client/src/components/Hero/hero.jsx
+++ Client/src/components/Hero/hero.jsx
@@ -1,13 +1,22 @@
-import videoBg from '../../assets/videoBg.webm'
-
+import { useState } from 'react'
+import video1 from '../../assets/video1.webm'
+import video2 from '../../assets/video2.webm'
 
 import './hero.css'
 
 const Hero = () => {
+  
+  
+    const [currentVideo, setCurrentVideo] = useState(1);
+  
+    const handleVideoEnd = () => {
+      // Switch between video 1 and video 2
+      setCurrentVideo(currentVideo === 1 ? 2 : 1);
+    };
   return (
     <div className="about-section">
         <div className="overlay"></div>
-        <video src={videoBg} autoPlay loop muted id="bg-video"/>
+        <video src={currentVideo===1?video1:video2}  onEnded={handleVideoEnd} autoPlay  muted id="bg-video"/>
     </div>
   )
 }
