484c221dad6effa9c182271d4f8ad7753f6633f5
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-02-26 19:26:19 +0530

    reviews added

--- Client/src/components/Reviews/reviews.jsx
+++ Client/src/components/Reviews/reviews.jsx
@@ -50,6 +50,105 @@ const Reviews = () => {
                             <p className="reviewersReview">"Outstanding cooking service! The meals were delicious and the chef was very professional."</p>
                         </div>
                     </div>
+                    <div className="review">
+                        <div className="avatar"></div>
+                        <div className="content-reviews">
+                            <h3 className="reviewerName">Sriya</h3>
+                            <div className="rating">
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                            </div>
+                            <p className="reviewersReview">"I have been using Homaid Services for years and I would not trust anyone else with my home!"</p>
+                        </div>
+                    </div>
+                    <div className="review">
+                        <div className="avatar"></div>
+                        <div className="content-reviews">
+                            <h3 className="reviewerName">Sandeep</h3>
+                            <div className="rating">
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                            </div>
+                            <p className="reviewersReview">
+                                "The cooking services from Homaid are a game-changer! Delicious meals without the hassle of grocery shopping and cooking"
+                            </p>
+                        </div>
+                    </div>
+                    <div className="review">
+                        <div className="avatar"></div>
+                        <div className="content-reviews">
+                            <h3 className="reviewerName">Dhruva</h3>
+                            <div className="rating">
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                            </div>
+                            <p className="reviewersReview">
+                                "Homaid professional maids are thorough, reliable, and friendly. I couldnot be happier with their service!"
+                            </p>
+                        </div>
+                    </div>
+                    <div className="review">
+                        <div className="avatar"></div>
+                        <div className="content-reviews">
+                            <h3 className="reviewerName">Ananya</h3>
+                            <div className="rating">
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                            </div>
+                            <p className="reviewersReview">
+                                "Homaid Services is simply unmatched. Their cleaning prowess and culinary skills have made my life easier 
+                                and my home happier. I am a loyal customer for a reason"
+                            </p>
+                        </div>
+                    </div>
+                    <div className="review">
+                        <div className="avatar"></div>
+                        <div className="content-reviews">
+                            <h3 className="reviewerName">Ananya</h3>
+                            <div className="rating">
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                            </div>
+                            <p className="reviewersReview">
+                                "Trust is paramount when it comes to home services, and Homaid excels in delivering it. Their 
+                                disciplined and trained staff not only keep my home impeccably clean but also operate with
+                                transparency, creating a partnership built on reliability and excellence."
+                            </p>
+                        </div>
+                    </div>
+                    <div className="review">
+                        <div className="avatar"></div>
+                        <div className="content-reviews">
+                            <h3 className="reviewerName">Nishita</h3>
+                            <div className="rating">
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                                <span className="star">&#9733;</span>
+                            </div>
+                            <p className="reviewersReview">
+                                "When it comes to cooking, Homaid Services is in a league of its own. Their chefs have a magic touch that transforms 
+                                ordinary meals into extraordinary experiences. I would not let anyone else take the reins in my kitchen."
+                            </p>
+                        </div>
+                    </div>
+
                     {/* Add more reviews here */}
                 </div>
             </div>
