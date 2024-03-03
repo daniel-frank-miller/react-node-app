93e4f7b3f4086d4171516e673f01860797d082f8
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-03-03 20:33:08 +0530

    updated packages

--- Client/src/components/Quotation/index.jsx
+++ Client/src/components/Quotation/index.jsx
@@ -8,7 +8,7 @@ const Quotation = ()=>(
             <p className='quotation-description'>Contact to obtain the best solution for deburring and polishing application.</p>
         </div>
         <Link to="/contactus">
-            <button type='button' className='contact-us-button'>Contact Us</button>
+            <button type='button' className='contact-us-button'>ContactUs</button>
         </Link>
     </div>
 )
