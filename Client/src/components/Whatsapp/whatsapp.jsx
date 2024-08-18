c733ea251e1772f02abfb07685c9ed36aab10e86
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-08-18 13:17:11 +0530

    Updated UI Components

--- Client/src/components/Whatsapp/whatsapp.jsx
+++ Client/src/components/Whatsapp/whatsapp.jsx
@@ -1,21 +1,24 @@
 import "/src/components/Whatsapp/whatsapp.css";
 import { SiWhatsapp } from "react-icons/si";
-import '@fortawesome/fontawesome-free/css/all.css';
-
+import "@fortawesome/fontawesome-free/css/all.css";
 
 const Whatsapp = () => {
-  const handleWhatsappClick = (event) => {
-    event.preventDefault();
-    // const message = encodeURIComponent("Hello Homaid! i need help");
-    window.open('https://wa.me/8125522213', '_blank');
-  };
+  // const handleWhatsappClick = (event) => {
+  //   event.preventDefault();
+  //   // const message = encodeURIComponent("Hello Homaid! i need help");
+  //   window.open("https://wa.me/8125522213", "_blank");
+  // };
 
   return (
     <div className="whatsapp_container">
-      	<a href="https://wa.me/8125522213" className="btn-whatsapp-pulse" target="_blank">
-       {/* <SiWhatsapp /> */}
-       <i className="fab fa-whatsapp"></i>
-    </a>
+      <a
+        href="https://wa.me/8125522213"
+        className="btn-whatsapp-pulse"
+        target="_blank" rel="noreferrer"
+      >
+        <SiWhatsapp />
+        <i className="fab fa-whatsapp"></i>
+      </a>
     </div>
   );
 };
